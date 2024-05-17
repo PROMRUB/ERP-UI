c36504591f394a57ecfa05f62ed73a604a647624
Thanakarn Klangkasame <77600906+Simulationable@users.noreply.github.com>
2024-05-17 20:49:09 +0700

    [Update] Home Page.
    Updated Home Page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,19 +3,38 @@
 
 <template>
   <main>
+    <div class="row">
+      <button class="general-btn" :class="{ active: generalIsActive }" @click="general">
+        <span><i class="fa fa-file-text-o fa-lg"></i>ข้อมูลทั่วไป</span>
+      </button>
+      <button class="address-btn" :class="{ active: addressIsActive }" @click="address">
+        <span><i class="fa fa-map-o fa-lg"></i>ที่อยู่</span>
+      </button>
+    </div>
+    <div class="row" v-if="generalIsActive">
+      <GeneralInformation />
+    </div>
+    <div class="row" v-if="addressIsActive">
+      <AddressInformation />
+    </div>
   </main>
 </template>
 
 <script>
 
+import GeneralInformation from '@/components/Home/GeneralInformation.vue'
+import AddressInformation from '@/components/Home/AddressInformation.vue'
 import { useProfileStore } from '@/stores/ProfileStore'
 
 export default {
   components: {
+    GeneralInformation,
+    AddressInformation
   },
   data() {
     return {
-      isSignIn: false,
+      generalIsActive: true,
+      addressIsActive: false,
       profileStore: useProfileStore()
     };
   },
@@ -35,7 +54,61 @@ export default {
       else {
         this.profileStore.isSignIn = true
       }
+    },
+    general() {
+      this.generalIsActive = true
+      this.addressIsActive = false
+    },
+    address() {
+      this.generalIsActive = false
+      this.addressIsActive = true
     }
   }
 };
 </script>
+
+<style>
+.general-btn {
+  position: absolute;
+  width: 200px;
+  height: 48px;
+  left: 350px;
+  top: 15%;
+  background: #384577;
+  border-radius: 4px;
+  color: white;
+  padding-top: 2px;
+  padding-bottom: 12px;
+  cursor: pointer;
+  text-align: center;
+  border: 1px solid #384577;
+}
+
+.active {
+  background: #00275E;
+  border: 1px solid #00275E;
+  color: white;
+}
+
+.address-btn {
+  position: absolute;
+  width: 200px;
+  height: 48px;
+  left: 550px;
+  top: 15%;
+  background: #384577;
+  border-radius: 4px;
+  color: white;
+  padding-top: 2px;
+  padding-bottom: 12px;
+  cursor: pointer;
+  text-align: center;
+  border: 1px solid #384577;
+}
+
+.active {
+  background: #00275E;
+  border: 1px solid #00275E;
+  color: white;
+}
+</style>
