d2a5a04546e56465127fcd64d873c812abad1b98
Thanakarn Klangkasame <77600906+Simulationable@users.noreply.github.com>
2024-05-23 13:22:07 +0700

    [Update] check undefined.
    Updated check undefined.

--- src/views/BusinessView.vue
+++ src/views/BusinessView.vue
@@ -43,6 +43,8 @@ export default {
         this.$router.push('/signin')
       } else {
         this.profileStore.isSignIn = true
+        const profileData = await this.profileStore.fetchProfile()
+        const businessData = await this.profileStore.fetchBusiness()
         let business = await this.businessStore.fetchBusiness(
           this.profileStore.profile.orgCustomId,
           this.profileStore.businessId
