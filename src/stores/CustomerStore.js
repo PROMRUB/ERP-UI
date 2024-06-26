31d198ffbf6c7327d8c08134a928a513e5c846f6
Thanakarn Klangkasame <thanakarn.klangkasame@gmail.com>
2024-06-26 14:34:31 +0700

    [Update] Config.
    Config File Updated.

--- src/stores/CustomerStore.js
+++ src/stores/CustomerStore.js
@@ -9,9 +9,9 @@ export const useCustomerStore = defineStore('customerStore', () => {
   }
 
   let selectedCustomer = {}
-  
-  let baseUrl = 'https://localhost:44345'
-  // let baseUrl = 'https://sales-api-dev.prom.co.th'
+
+  // let baseUrl = 'https://localhost:44345'
+  let baseUrl = 'https://sales-api-dev.prom.co.th'
 
   let axiosExport = axios.create({
     baseUrl
