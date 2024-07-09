0be754c3010c17cc0cdc5f331de7e612e22f9b8b
Thanakarn Klangkasame <thanakarn.klangkasame@gmail.com>
2024-07-10 02:40:50 +0700

    [Update] Product Page.
    Product Page Updated.

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -2,19 +2,37 @@
 
 <template>
   <main>
-    <div v-if="!hvData">
+    <div v-if="!productStore.hvData">
       <img class="no-data" src="@/assets/no-data.png" alt="No Data" /><br />
       <div class="no-data-label">
         <span>ขณะนี้ยังไม่มีข้อมูลใดๆของคุณอยู่ในระบบ</span>
       </div>
     </div>
     <div v-else>
-      <ProductTable />
+      <div v-if="productListActive">
+        <ProductTable @pageControl="pageControl" />
+      </div>
+      <div v-if="productInformationActive">
+        <div class="row">
+          <button
+            class="product-btn"
+            :class="{ active: productActive }"
+            @click="pageControl(`product`)"
+          >
+            <span><i class="fa fa-file-text-o fa-lg"></i>สินค้า</span>
+          </button>
+        </div>
+        <div class="row" v-if="productActive">
+          <ProductInformation @pageControl="pageControl" @saveproduct="saveproduct" />
+        </div>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
+import ProductInformation from '@/components/Product/ProductInformation.vue'
+
 import { useProfileStore } from '@/stores/ProfileStore'
 import { useProductStore } from '@/stores/ProductStore'
 
@@ -22,14 +40,22 @@ import ProductTable from '@/components/Product/ProductTable.vue'
 
 export default {
   components: {
-    ProductTable
+    ProductTable,
+    ProductInformation
   },
   data() {
     return {
-      hvData: true,
+      productListActive: true,
+      productInformationActive: false,
+      productActive: false,
+      refresh: true,
       profileStore: useProfileStore(),
       productStore: useProductStore()
     }
+  },
+  watch: {
+    refresh() {},
+    productActive(newValue, oldValue) {}
   },
   mounted() {
     this.updateComponent()
@@ -40,6 +66,10 @@ export default {
   methods: {
     async updateComponent() {
       this.$emit('loading')
+      if (this.refresh) {
+        sessionStorage.setItem('page', 'productList')
+        this.refresh = true
+      }
       sessionStorage.setItem('changeBusiness', 'true')
       let token = sessionStorage.getItem('token')
       if (token == '' || token == undefined || token == null) {
@@ -51,20 +81,88 @@ export default {
         if (this.profileStore.businessList.length == 0) {
           const businessData = await this.profileStore.fetchBusiness()
         }
-        const customers = await this.productStore.fetchProductList(
+        const products = await this.productStore.fetchProductList(
           this.profileStore.profile.orgCustomId,
           this.profileStore.businesskey
         )
         if (sessionStorage.getItem('page') == 'productList') {
-          if (customers.length > 0) {
-            this.customerStore.hvData = true
+          if (products.length > 0) {
+            this.productStore.hvData = true
           } else {
-            this.customerStore.hvData = false
+            this.productStore.hvData = false
           }
         }
         this.$emit('loaded')
       }
+    },
+    async pageControl(pageName) {
+      sessionStorage.setItem('page', pageName)
+      this.$emit('loading')
+      if (pageName == 'productList') {
+        this.productListActive = true
+        this.productInformationActive = false
+        this.productActive = false
+      } else if (pageName == 'productEntry') {
+        sessionStorage.setItem('rendered', 'false')
+        this.productListActive = false
+        this.productInformationActive = true
+        this.productActive = true
+        sessionStorage.setItem('mode', 'Entry')
+        sessionStorage.setItem('changeBusiness', 'false')
+      } else if (pageName == 'productInquiry') {
+        const catData = await this.productStore.fetchCatList(
+          this.profileStore.profile.orgCustomId,
+          this.profileStore.businesskey
+        )
+        this.productListActive = false
+        this.productInformationActive = true
+        this.productActive = true
+        let selectedProduct = this.productStore.selectedProduct
+        this.productStore.selectedProduct = this.productStore.productList.find(
+          (item) => item.productCustomId === selectedProduct
+        )?.productId
+        if (this.productStore.selectedProduct) {
+          this.productStore.fetchProductbyId(
+            this.profileStore.profile.orgCustomId,
+            this.profileStore.businesskey,
+            this.productStore.selectedProduct
+          )
+        }
+        sessionStorage.setItem('changeBusiness', 'false')
+        sessionStorage.setItem('mode', 'Inquiry')
+        this.showContact = true
+      } else if (pageName == 'product') {
+        this.productListActive = false
+        this.productInformationActive = true
+        this.productActive = true
+        sessionStorage.setItem('changeBusiness', 'false')
+      }
+      this.$emit('loaded')
     }
   }
 }
 </script>
+
+<style>
+.product-btn {
+  position: absolute;
+  width: 200px;
+  height: 48px;
+  left: 350px;
+  top: 15%;
+  background: #384577;
+  border-radius: 4px;
+  color: white;
+  padding-top: 2px;
+  padding-bottom: 12px;
+  cursor: pointer;
+  text-align: center;
+  border: 1px solid #384577;
+}
+
+.active {
+  background: #00275e;
+  border: 1px solid #00275e;
+  color: white;
+}
+</style>
