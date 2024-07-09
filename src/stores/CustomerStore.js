c935e616173ac34bf7839b007a5dd572e04cb9d4
Thanakarn Klangkasame <thanakarn.klangkasame@gmail.com>
2024-07-10 02:41:43 +0700

    [Update] API Route.
    API Route Updated.

--- src/stores/CustomerStore.js
+++ src/stores/CustomerStore.js
@@ -9,10 +9,10 @@ export const useCustomerStore = defineStore('customerStore', () => {
   let customerProfile = {}
   let contactProfile = {}
   let selectedCustomer = {}
-
-  let baseUrl = 'https://localhost:44345'
-  // let baseUrl = 'https://sales-api-dev.prom.co.th'
   
+  // let baseUrl = 'https://localhost:44345'
+  let baseUrl = 'https://sales-api-dev.prom.co.th'
+
   let axiosExport = axios.create({
     baseUrl
   })
